bfb530d96e152a66e82c18ece7c9b6d0a52a11a0
Rong Shen <rong.shen@nxmix.com>
2012-10-21 13:59:27 +0800

    Deleted unecessary modules

--- example/server.js
+++ example/server.js
@@ -65,36 +65,6 @@ everyauth.everymodule
     callback(null, usersById[id]);
   });
   
-everyauth.dailycred
-  .appId(conf.dc.appId)
-  .findOrCreateUser( function (session, accessToken, accessTokenExtra, dcUserMetadata) {
-    return usersByDcId[dcUserMetadata.id] ||
-      (usersByDcId[dcUserMetadata.id] = addUser('dailycred', dcUserMetadata));
-  })
-  .redirectPath('/');
-  
-everyauth.azureacs
-  .identityProviderUrl('https://acssample1.accesscontrol.windows.net/v2/wsfederation/')
-  .entryPath('/auth/azureacs')
-  .callbackPath('/auth/azureacs/callback')
-  .signingKey('d0julb9JNbCB8J2ACHzxU33SSiqbylQveQtuwOEvz24=')
-  .realm('urn:nodeacslocal')
-  .homeRealm('')
-  .tokenFormat('swt')
-  .findOrCreateUser( function (session, acsUser) {
-     return usersByAzureAcs[acsUser.id] || (usersByAzureAcs[acsUser.id] = addUser('azureAcs', acsUser));
-  })
-  .redirectPath('/');
-
-everyauth
-  .openid
-    .myHostname('http://local.host:3000')
-    .findOrCreateUser( function (session, userMetadata) {
-      return usersByOpenId[userMetadata.claimedIdentifier] ||
-        (usersByOpenId[userMetadata.claimedIdentifier] = addUser('openid', userMetadata));
-    })
-    .redirectPath('/');
-
 
 everyauth
   .facebook
@@ -103,15 +73,6 @@ everyauth
     .findOrCreateUser( function (session, accessToken, accessTokenExtra, fbUserMetadata) {
       return usersByFbId[fbUserMetadata.id] ||
         (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata));
-    })
-    .redirectPath('/');
-
-everyauth
-  .twitter
-    .consumerKey(conf.twit.consumerKey)
-    .consumerSecret(conf.twit.consumerSecret)
-    .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
-      return usersByTwitId[twitUser.id] || (usersByTwitId[twitUser.id] = addUser('twitter', twitUser));
     })
     .redirectPath('/');
 
@@ -178,50 +139,6 @@ everyauth
     .loginSuccessRedirect('/')
     .registerSuccessRedirect('/');
 
-everyauth.github
-  .appId(conf.github.appId)
-  .appSecret(conf.github.appSecret)
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, ghUser) {
-      return usersByGhId[ghUser.id] || (usersByGhId[ghUser.id] = addUser('github', ghUser));
-  })
-  .redirectPath('/');
-
-everyauth.instagram
-  .appId(conf.instagram.clientId)
-  .appSecret(conf.instagram.clientSecret)
-  .scope('basic')
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, hipster) {
-      return usersByInstagramId[hipster.id] || (usersByInstagramId[hipster.id] = addUser('instagram', hipster));
-  })
-  .redirectPath('/');
-
-everyauth.foursquare
-  .appId(conf.foursquare.clientId)
-  .appSecret(conf.foursquare.clientSecret)
-  .findOrCreateUser( function (sess, accessTok, accessTokExtra, addict) {
-      return usersByFoursquareId[addict.id] || (usersByFoursquareId[addict.id] = addUser('foursquare', addict));
-  })
-  .redirectPath('/');
-
-everyauth.gowalla
-  .appId(conf.gowalla.apiKey)
-  .appSecret(conf.gowalla.apiSecret)
-  .moduleErrback( function(err) {
-    console.log("moduleErrback for Gowalla", err);
-  })
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, loser) {
-    return usersByGowallaId[loser.url] || (usersByGowallaId[loser.url] = addUser('gowalla', loser));
-  })
-  .redirectPath('/');
-
-everyauth.linkedin
-  .consumerKey(conf.linkedin.apiKey)
-  .consumerSecret(conf.linkedin.apiSecret)
-  .findOrCreateUser( function (sess, accessToken, accessSecret, linkedinUser) {
-    return usersByLinkedinId[linkedinUser.id] || (usersByLinkedinId[linkedinUser.id] = addUser('linkedin', linkedinUser));
-  })
-  .redirectPath('/');
-
 everyauth.google
   .appId(conf.google.clientId)
   .appSecret(conf.google.clientSecret)
@@ -233,205 +150,6 @@ everyauth.google
   })
   .redirectPath('/');
 
-everyauth.angellist
-  .appId(conf.angellist.clientId)
-  .appSecret(conf.angellist.clientSecret)
-  .findOrCreateUser( function (sess, accessToken, extra, angellistUser) {
-    angellistUser.refreshToken = extra.refresh_token;
-    angellistUser.expiresIn = extra.expires_in;
-    return usersByAngelListId[angellistUser.id] || (usersByAngelListId[angellistUser.id] = addUser('angellist', angellistUser));
-  })
-  .redirectPath('/');
-
-everyauth.yahoo
-  .consumerKey(conf.yahoo.consumerKey)
-  .consumerSecret(conf.yahoo.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessSecret, yahooUser) {
-    return usersByYahooId[yahooUser.id] || (usersByYahooId[yahooUser.id] = addUser('yahoo', yahooUser));
-  })
-  .redirectPath('/');
-
-everyauth.googlehybrid
-  .myHostname('http://local.host:3000')
-  .consumerKey(conf.googlehybrid.consumerKey)
-  .consumerSecret(conf.googlehybrid.consumerSecret)
-  .scope(['http://docs.google.com/feeds/','http://spreadsheets.google.com/feeds/'])
-  .findOrCreateUser( function(session, userAttributes) {
-    return usersByGoogleHybridId[userAttributes.claimedIdentifier] || (usersByGoogleHybridId[userAttributes.claimedIdentifier] = addUser('googlehybrid', userAttributes));
-  })
-  .redirectPath('/');
-
-everyauth.readability
-  .consumerKey(conf.readability.consumerKey)
-  .consumerSecret(conf.readability.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessSecret, reader) {
-      return usersByReadabilityId[reader.username] || (usersByReadabilityId[reader.username] = addUser('readability', reader));
-  })
-  .redirectPath('/');
-
-everyauth
-  .dropbox
-    .consumerKey(conf.dropbox.consumerKey)
-    .consumerSecret(conf.dropbox.consumerSecret)
-    .findOrCreateUser( function (sess, accessToken, accessSecret, dropboxUserMetadata) {
-      return usersByDropboxId[dropboxUserMetadata.uid] ||
-        (usersByDropboxId[dropboxUserMetadata.uid] = addUser('dropbox', dropboxUserMetadata));
-    })
-    .redirectPath('/')
-
-everyauth.vimeo
-	.consumerKey(conf.vimeo.consumerKey)
-	.consumerSecret(conf.vimeo.consumerSecret)
-	.findOrCreateUser( function (sess, accessToken, accessSecret, vimeoUser) {
-		return usersByVimeoId[vimeoUser.id] ||
-      (usersByVimeoId[vimeoUser.id] = addUser('vimeo', vimeoUser));
-	})
-	.redirectPath('/')
-
-everyauth.justintv
-  .consumerKey(conf.justintv.consumerKey)
-  .consumerSecret(conf.justintv.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessSecret, justintvUser) {
-    return usersByJustintvId[justintvUser.id] ||
-      (usersByJustintvId[justintvUser.id] = addUser('justintv', justintvUser));
-  })
-  .redirectPath('/')
-
-everyauth['37signals']
-  .appId(conf['_37signals'].clientId)
-  .appSecret(conf['_37signals'].clientSecret)
-  .findOrCreateUser( function (sess, accessToken, accessSecret, _37signalsUser) {
-    return usersBy37signalsId[_37signalsUser.id] ||
-      (usersBy37signalsId[_37signalsUser.identity.id] = addUser('37signals', _37signalsUser));
-  })
-  .redirectPath('/')
-
-everyauth.tumblr
-  .consumerKey(conf.tumblr.consumerKey)
-  .consumerSecret(conf.tumblr.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessSecret, tumblrUser) {
-    return usersByTumblrName[tumblrUser.name] ||
-      (usersByTumblrName[tumblrUser.name] = addUser('tumblr', tumblrUser));
-  })
-  .redirectPath('/');
-
-everyauth.box
-  .apiKey(conf.box.apiKey)
-  .findOrCreateUser( function (sess, authToken, boxUser) {
-    return usersByBoxId[boxUser.user_id] ||
-      (usersByDropboxId[boxUser.user_id] = addUser('box', boxUser));
-  })
-  .redirectPath('/');
-
-everyauth.dwolla
-  .appId(conf.dwolla.clientId)
-  .appSecret(conf.dwolla.clientSecret)
-  .scope('accountinfofull')
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, dwollaUser) {
-    return usersByDwollaId[dwollaUser.id] || (usersByDwollaId[dwollaUser.id] = addUser('dwolla', dwollaUser));
-  })
-  .redirectPath('/');
-
-everyauth.vkontakte
-  .appId(conf.vkontakte.appId)
-  .appSecret(conf.vkontakte.appSecret)
-  .findOrCreateUser( function (session, accessToken, accessTokenExtra, vkUserMetadata) {
-    return usersByVkId[vkUserMetadata.uid] ||
-      (usersByVkId[vkUserMetadata.uid] = addUser('vkontakte', vkUserMetadata));
-  })
-  .redirectPath('/');
-
-everyauth.mailru
-  .appId(conf.mailru.appId)
-  .appSecret(conf.mailru.appSecret)
-  .findOrCreateUser( function (session, accessToken, accessTokenExtra, mlUserMetadata) {
-    return usersMailruId[mlUserMetadata.uid] ||
-      (usersMailruId[mlUserMetadata.uid] = addUser('mailru', mlUserMetadata));
-  })
-  .redirectPath('/');
-
-everyauth.skyrock
-  .consumerKey(conf.skyrock.consumerKey)
-  .consumerSecret(conf.skyrock.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, skyrockUser) {
-    return usersBySkyrockId[skyrockUser.id_user] || (usersBySkyrockId[skyrockUser.id_user] = addUser('skyrock', skyrockUser));
-  })
-  .redirectPath('/');
-
-everyauth.evernote
-  .oauthHost(conf.evernote.oauthHost)
-  .consumerKey(conf.evernote.consumerKey)
-  .consumerSecret(conf.evernote.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, enUserMetadata) {
-    return usersByEvernoteId[enUserMetadata.userId] || (usersByEvernoteId[enUserMetadata.userId] = addUser('evernote', enUserMetadata));
-  })
-  .redirectPath('/');
-
-everyauth.tripit
-  .consumerKey(conf.tripit.consumerKey)
-  .consumerSecret(conf.tripit.consumerSecret)
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, tripitProfile) {
-    var userId = tripitProfile['@attributes'].ref;
-    return usersByTripIt[userId] || (usersByTripIt[userId] = addUser('tripit', tripitProfile));
-  })
-  .redirectPath('/');
-
-everyauth['500px']
-  .consumerKey(conf._500px.consumerKey)
-  .consumerSecret(conf._500px.consumerSecret)
-  .findOrCreateUser(function(sess, accessToken, accessSecret, user) {
-    return usersBy500pxId[user.id] || (usersBy500pxId[user.id] = addUser('500px', user));
-  })
-  .redirectPath('/');
-
-everyauth.mendeley
-  .consumerKey(conf.mendeley.consumerKey)
-  .consumerSecret(conf.mendeley.consumerSecret)
-  .findOrCreateUser(function(sess, accessToken, accessSecret, user) {
-    return usersByMendeleyId[user.main.profile_id] || (usersByMendeleyId[user.main.profile_id] = addUser('mendeley', user));
-  })
-  .redirectPath('/');
-
-everyauth.runkeeper
-  .appId(conf.runkeeper.appId)
-  .appSecret(conf.runkeeper.appSecret)
-  .findOrCreateUser(function(sess, accessToken, accessSecret, user) {
-    return usersByRunKeeperId[user.userID] || (usersByRunKeeperId[user.userID] = addUser('runkeeper', user));
-  })
-  .redirectPath('/');
-
-everyauth
-  .soundcloud
-    .appId(conf.soundcloud.appId)
-    .appSecret(conf.soundcloud.appSecret)
-    .findOrCreateUser( function (sess, accessToken, accessTokenExtra, soundcloudUser) {
-      return usersBySoundCloudId[soundcloudUser.id] || (usersBySoundCloudId[soundcloudUser.id] = addUser('soundcloud', soundcloudUser));
-    })
-    .redirectPath('/');
-
-everyauth
-  .mixi
-    .appId(conf.mixi.consumerKey)
-    .appSecret(conf.mixi.consumerSecret)
-    .scope(conf.mixi.scope)
-    .display('pc')
-    .findOrCreateUser( function (session, accessToken, accessTokenExtra, mixiUserMetadata) {
-      return usersByFbId[mixiUserMetadata.id] ||
-        (usersByFbId[mixiUserMetadata.id] = addUser('mixi', mixiUserMetadata));
-    })
-    .redirectPath('/');
-
-everyauth
-  .mailchimp
-    .appId(conf.mailchimp.appId)
-    .appSecret(conf.mailchimp.appSecret)
-    .myHostname(process.env.HOSTNAME || "http://127.0.0.1:3000")//MC requires 127.0.0.1 for dev
-    .findOrCreateUser( function (session, accessToken, accessTokenExtra, mailchimpUser){
-      return usersByMailchimpId[mailchimpUser.id] ||
-        (usersByMailchimpId[mailchimpUser.user_id] = addUser('mailchimp', mailchimpUser));
-    })
-    .redirectPath("/");
-    
 everyauth
   .weibo
     .appId(conf.weibo.appId)
@@ -441,22 +159,6 @@ everyauth
         (usersByWeiboId[weiboUser.uid] = addUser('weibo', weiboUser));
     })
     .redirectPath("/");
-
-everyauth.meetup
-  .appId(conf.meetup.appId)
-  .appSecret(conf.meetup.appSecret)
-  .findOrCreateUser(function(sess, accessToken, accessSecret, user) {
-    return usersByMeetupId[user.id] || (usersByMeetupId[user.id] = addUser('meetup', user));
-  })
-  .redirectPath('/');
-
-everyauth.viadeo
-  .appId(conf.viadeo.clientId)
-  .appSecret(conf.viadeo.clientSecret)
-  .findOrCreateUser( function (sess, accessToken, accessTokenExtra, viadeoUser) {
-    return usersByViadeoId[viadeoUser.id] || (usersByViadeoId[viadeoUser.id] = addUser('viadeo', viadeoUser));
-  })
-  .redirectPath('/');
 
 var app = express.createServer(
     express.bodyParser()
